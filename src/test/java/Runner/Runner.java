878dfeadf31fbd598d8e1f22fd539a423c52dfd6
Ismail KILGOZ <ligerhyme@hotmail.com>
2024-12-20 21:50:15 +0300

    ismail/20.12/v3

--- src/test/java/Runner/Runner.java
+++ src/test/java/Runner/Runner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
                 "json:target/cucumber.json",},
         features = "src/test/resources/features",
         glue = "stepdefinitions",
-        tags = " @Add" ,
+        tags = " @iso" ,
         dryRun = true
 
 
