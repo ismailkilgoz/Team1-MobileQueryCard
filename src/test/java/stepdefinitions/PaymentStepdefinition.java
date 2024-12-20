314941f3b0c3988726121092d7ba0664f39eeb45
seren <serendnmez@gmail.com>
2024-12-20 18:30:54 +0100

    V

--- src/test/java/stepdefinitions/PaymentStepdefinition.java
+++ src/test/java/stepdefinitions/PaymentStepdefinition.java
@@ -8,8 +8,8 @@ public class PaymentStepdefinition {
 
     ExcelDataReader excelDataReader=new ExcelDataReader(ConfigReader.getProperty("userAccess"));
 
-    @Given("The user is signed in")
+    @Given("The user is signed in as cusmoter seren")
     public void the_user_is_signed_in() {
-       excelDataReader.signInWithExcelData(excelDataReader,6,2);
+       excelDataReader.signInWithExcelData(excelDataReader,6,5);
     }
 }
