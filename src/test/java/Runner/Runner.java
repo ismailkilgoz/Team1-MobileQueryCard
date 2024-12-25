45b5779df88c79d53db5f7a599dfda9aa75cb576
Umit URETEN <uretenumit@gmail.com>
2024-12-25 13:06:39 +0300

    umit/20.12.2024/v4

--- src/test/java/Runner/Runner.java
+++ src/test/java/Runner/Runner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
                 "json:target/cucumber.json",},
         features = "src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@US_016" ,
+        tags = "@US_017" ,
         dryRun = false
 
 
